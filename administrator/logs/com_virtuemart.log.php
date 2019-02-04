0243d6ad74f206fc17da0afa937dc5b8ab113af0
root <1530416@upv.edu.mx>
2019-02-04 18:38:52 +0000

    commit1

--- administrator/logs/com_virtuemart.log.php
+++ administrator/logs/com_virtuemart.log.php
@@ -46,3 +46,51 @@
 2019-02-01 01:33:20 ERROR vmError: Couldnt create thumb, file not found C:\wamp64\www\estebanlucasv\images\joomlashine\sample\stories\virtuemart\product\dress-4.14.jpg
 2019-02-01 01:33:20 ERROR vmError: Couldnt create thumb, file not found C:\wamp64\www\estebanlucasv\images\joomlashine\sample\stories\virtuemart\product\dress-2.1.jpg
 2019-02-01 01:33:20 ERROR vmError: Couldnt create thumb, file not found C:\wamp64\www\estebanlucasv\images\joomlashine\sample\stories\virtuemart\product\boy-515.jpg
+2019-02-02 10:58:32 ERROR vmError: Warning, the Safe Path is &lt;b&gt;not accessible&lt;/b&gt; (does not exist or no permission). Use this link to the &lt;a href='http://www.estebanlucasv.me/frerado-es-me/administrator/index.php?option=com_virtuemart&amp;view=updatesmigration&amp;show_spwizard=1' &gt;setup wizard&lt;/a&gt;
+2019-02-02 10:58:42 ERROR vmError: COM_VIRTUEMART_CANNOT_STORE_CONFIG
+2019-02-02 13:51:34 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/nik-macmillan-284747.jpg
+2019-02-02 13:51:34 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/senjuti-kundu-349426.jpg
+2019-02-02 13:51:34 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/nick-karvounis-423669 2.jpg
+2019-02-02 13:51:34 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/mohammed-hijas-334501.jpg
+2019-02-02 13:51:34 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/sale 1.png
+2019-02-02 13:51:34 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/product_09 copy.jpg
+2019-02-02 13:51:34 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/download.jpg
+2019-02-02 13:51:34 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/download-2.jpg
+2019-02-02 13:51:34 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/download-4.jpg
+2019-02-02 13:51:34 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/download-5.jpg
+2019-02-02 13:51:34 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/download-3.jpg
+2019-02-02 13:51:34 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/download-1.jpg
+2019-02-02 13:51:45 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/nik-macmillan-284747.jpg
+2019-02-02 13:51:45 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/senjuti-kundu-349426.jpg
+2019-02-02 13:51:45 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/nick-karvounis-423669 2.jpg
+2019-02-02 13:51:45 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/mohammed-hijas-334501.jpg
+2019-02-02 13:51:45 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/sale 1.png
+2019-02-02 13:51:45 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/product_09 copy.jpg
+2019-02-02 13:51:45 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/download.jpg
+2019-02-02 13:51:45 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/download-2.jpg
+2019-02-02 13:51:45 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/download-4.jpg
+2019-02-02 13:51:45 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/download-5.jpg
+2019-02-02 13:51:45 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/download-3.jpg
+2019-02-02 13:51:45 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/download-1.jpg
+2019-02-02 13:52:09 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/product_09 copy.jpg
+2019-02-02 13:59:13 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/nik-macmillan-284747.jpg
+2019-02-02 13:59:13 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/senjuti-kundu-349426.jpg
+2019-02-02 13:59:13 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/nick-karvounis-423669 2.jpg
+2019-02-02 13:59:13 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/sale 1.png
+2019-02-02 13:59:13 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/product_09 copy.jpg
+2019-02-02 13:59:13 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/seth-doyle-195684.jpg
+2019-02-02 15:04:38 ERROR vmError: Invalid media, image type does not fit to extension sql.png 2!=png
+2019-02-02 15:36:06 ERROR vmError: Invalid media, image type does not fit to extension Introduccion-a-la-Programacion.png 2!=png
+2019-02-03 06:41:43 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/nik-macmillan-284747.jpg
+2019-02-03 06:41:43 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/nick-karvounis-423669 2.jpg
+2019-02-03 06:41:43 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/mohammed-hijas-334501.jpg
+2019-02-03 06:41:43 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/sale 1.png
+2019-02-03 06:41:43 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/product_09 copy.jpg
+2019-02-03 06:41:43 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/download.jpg
+2019-02-03 06:41:43 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/download-2.jpg
+2019-02-03 06:41:43 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/download-4.jpg
+2019-02-03 06:41:43 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/download-5.jpg
+2019-02-03 06:41:43 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/download-3.jpg
+2019-02-03 06:41:43 ERROR vmError: Couldnt create thumb, file not found /var/www/html/frerado-es-me/images/joomlashine/sample/stories/virtuemart/category/download-1.jpg
+2019-02-03 10:07:28 ERROR vmError: The parameter &lt;em&gt;Merchant e-mail&lt;/em&gt; is required for the payment &lt;em&gt;paypal&lt;/em&gt; (&lt;em&gt;  3&lt;/em&gt;)
+2019-02-03 10:07:28 ERROR vmError: The parameter &lt;em&gt;Merchant e-mail&lt;/em&gt; is required for the payment &lt;em&gt;paypal&lt;/em&gt; (&lt;em&gt;  3&lt;/em&gt;)
--- administrator/logs/error.php
+++ administrator/logs/error.php
@@ -6,3 +6,9 @@
 #Fields: datetime	priority clientip	category	message
 2019-01-31T20:47:03+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
 2019-02-01T01:22:34+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-02-02T12:03:54+00:00	INFO 107.167.106.184	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-02-02T12:03:57+00:00	INFO 107.167.106.184	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-02-02T12:04:05+00:00	INFO 107.167.106.184	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-02-02T12:06:12+00:00	INFO 107.167.106.184	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-02-02T12:06:38+00:00	INFO 107.167.106.184	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-02-03T10:02:25+00:00	INFO 107.167.108.172	joomlafailure	Username and password do not match or you do not have an account yet.
